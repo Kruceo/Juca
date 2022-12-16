6fa0663a30bd87087c152e98471da9063acd6b7f
Kruceo <rafa2015krueger@gmail.com>
2022-12-16 13:06:50 -0300

    example update and tests

--- out_test.js
+++ out_test.js
@@ -1,56 +1,60 @@
-//test
-
-
-//test
-
-let i = 0;
-
-let resultsi = [];
-
-
-for (i; i < (window.array.length); i++) {
-
+(async function anonymous(
+    ) {
+    //test
+    
+    
+    //test
+    
+    let i = 0;
+    
+    let resultsi = [];
+    
+    
+    for(i;i< (window.filter.length) ;i++){
+       
     let l = 0;
-
+    
     let resultsl = [];
-
-
-    for (l; l < window.array[i].types.length; l++) {
-        //$VARI$
-
-
-        //$NEXT$
-        resultsl += `<h1>
-                            ${(window.array[i].types[l].type.name)}
-                        </h1>
-`
+    
+    
+    
+    for(l;l < window.filter[i].types.length;l++){
+    //$VARI$
+       
+    
+    //$NEXT$
+    resultsl += `<h1>
+                                ${(window.filter[i].types[l].type.name)}
+                            </h1>
+    `
     }
     //$SUB-H10$
-
-
-    resultsi += ` <div id="box" key="7">
-                <p> ${window.array[i].id}</p>
-                <div class="img-box">
-                    <img src="${window.array[i].sprites.other['official-artwork'].front_default}" alt="">
-                </div>
-                <div class="txt-box">
-
-                    <p>
-                        ${window.array[i].name}
-                    </p>
-
-                    <h3>
-                        Types:
-                    </h3>
-                    <div id="types">
-                        ${resultsl}
-
+    
+    
+    resultsi += ` <div id="box" watch="window.filter.length" style="background: linear-gradient(45deg, ${ (window.colors[window.filter[i].types[0].type.name])+' 20% ,'+(window.colors[    (   window.filter[i].types[1] ? window.filter[i].types[1] : 0    ) ]  )+ ' 120%' });" key="8">
+                    <p> ${ window.filter[i].id}</p>
+                    <div class="img-box">
+                        <img src="${ window.filter[i].sprites.other['official-artwork'].front_default }" alt="">
                     </div>
-                </div>
-            </div>`
-
-};
-
-//$SUB-DIV3$
-
-return resultsi
+                    <div class="txt-box">
+    
+                        <p>
+                            ${ window.filter[i].name }
+                        </p>
+    
+                        <h3>
+                            Types:
+                        </h3>
+                        <div id="types">
+                            ${resultsl}
+    
+                        </div>
+                    </div>
+                </div>`
+    
+    };
+    
+    //$SUB-DIV3$
+    
+    return resultsi
+    })
